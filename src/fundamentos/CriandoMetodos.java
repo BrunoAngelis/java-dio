5378339194849c9a8adb48762f11a052f6af47df
BrunoAngelis <bng.8@outlookcom>
2022-07-28 15:47:06 -0300

    Adicionando exemplos de metodos

---
+++ src/fundamentos/CriandoMetodos.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class CriandoMetodos {
+    public static void main(String[] args) {
+        //São funções, ações ou sub-rotinas dentro de nossas classes
+        //Padrão camelCase e deve ser nomeado com verbo ex: andar, somarImposto.
+        // calcularImprimir -> tomar cuidado pois o metodo tem apenas uma função como boa pratica
+
+        // Antes de criar o metodo se perguntar:
+        // Qual a proposta principal do método?
+        // - Voce deve ae pergutnar constantemente até compreender a real finalidade do mesmo.
+
+        //Qual o tipo de retorno esperado após executar o método?
+        //- Voce deve analisar se o método será responsável por retornar algum valor.
+    }
+}
