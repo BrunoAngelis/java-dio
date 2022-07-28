a8a89a887ae7ad14b60d416f90d9fb6e540b0bc1
BrunoAngelis <bng.8@outlookcom>
2022-07-28 20:02:01 -0300

    Adicionado tipo de Escopos

---
+++ src/fundamentos/TiposEscopo.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class TiposEscopo {
+    public static void main(String[] args) {
+        //ESCOPO - "VISIBILIDADE" - É aonde nossas variaveis são declaradas e até aonde podemos acessar as mesmas.
+        // Ex: caso você declare uma variavel dentro de um metodo, o escopo dela esta limitado apenas dentro do metodo.
+
+        //Escopo de Classe
+        //Escopo de Metodo
+        //Escopo de Fluxo
+    }
+}
