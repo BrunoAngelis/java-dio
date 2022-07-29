53fc9168108ab58359fadd9046e6b9c948637e55
BrunoAngelis <bng.8@outlookcom>
2022-07-29 00:30:53 -0300

    Adicionado exemplos de palavras reservadas

---
+++ src/fundamentos/PalavrasReservadas.java
@@ -0,0 +1,55 @@
+package fundamentos;
+
+public class PalavrasReservadas {
+    //Palavras Reservadas
+
+    /*
+        Controle de Pacotes
+        - import
+        - package
+    */
+
+    /*
+        Modificadores de acesso
+        - public
+        - private
+        - protected
+     */
+
+    /*
+        Primitivos
+        - boolean
+        - byte
+        - char
+        - double
+        - float
+        - int
+        - long
+        - short
+        - void
+     */
+
+    /*
+        Modificadores de classes
+        - abstract
+        - class
+        - extends
+        - final
+        - implements
+        - interface
+        - native
+        - new
+        - static
+        - strictfp
+        - synchronized
+        - transient
+        - volatile
+     */
+
+    /*
+        Controle de fluxo dentro de um bloco de código
+        - break
+        - case
+        - continue
+     */
+}
