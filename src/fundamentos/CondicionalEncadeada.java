3412a03a8e25cf4ef31b1d9e3a034f9ace248e16
BrunoAngelis <bng.8@outlookcom>
2022-07-30 20:52:56 -0300

    Adicionado exemplos de estrutura condicional encadeada

---
+++ src/fundamentos/CondicionalEncadeada.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class CondicionalEncadeada {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if(nota > 7)
+            System.out.println("Aprovado");
+        else if (nota>5 && nota<7)
+            System.out.println("Recuperaçao");
+        else
+            System.out.println("Reprovado");
+
+    }
+}
