90efdda646ef3be9c6d749498e7809ee53953d54
BrunoAngelis <bng.8@outlookcom>
2022-07-30 20:57:55 -0300

    Adicionado exemplos de estrutura condicional encadeada

---
+++ src/fundamentos/CondiçãoTernaria.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class CondiçãoTernaria {
+    public static void main(String[] args) {
+        int nota = 7;
+
+        String resultado = nota>=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
