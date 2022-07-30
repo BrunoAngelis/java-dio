d64e211e69a3a884cf82d2ab626e7b578b6213ab
BrunoAngelis <bng.8@outlookcom>
2022-07-30 20:45:04 -0300

    Adicionado exemplos de estrutura condicional composta

--- src/fundamentos/CondicionalSimples.java
+++ src/fundamentos/CondicionalSimples.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CondicionalSimples {
     public static void main(String[] args) {
 
-        double saldo = 500.0;
+        double saldo = 100.0;
         double valorSolicitado = 150.0;
 
         if (saldo > valorSolicitado)
