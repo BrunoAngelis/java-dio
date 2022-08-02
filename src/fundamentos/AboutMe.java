79da5fefc3b5764708c39c303cb791b2a23e1ed3
BrunoAngelis <bng.8@outlookcom>
2022-08-02 20:24:04 -0300

    Adicionado exemplos de Tratamentos de Excessões

--- src/fundamentos/AboutMe.java
+++ src/fundamentos/AboutMe.java
@@ -1,16 +1,37 @@
 package fundamentos;
 
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
+         try {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        System.out.printf("Digite seu nome: ");
+        String nome = scanner.next();
 
-        System.out.println("Ola, me chamo" + nome + " " + sobrenome);
-        System.out.println("Tenho "+ idade +" anos ");
-        System.out.println("Minha altura é " + altura + "cm");
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
 
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Meu nome é: " + nome + " " + sobrenome);
+        System.out.println("Minha idade é: " + idade);
+         }
+         catch (InputMismatchException e){
+             System.out.println("Os campos idade e alutura precisam ser numericos");
+         }
     }
+
+    /*
+        Excecões mais comuns:
+        NullPointerException - Quando tentamos obter alguma informação de uma variavel nula.
+        ArithmeticException - Quando tentamos dividir um valor por zero
+        SQLException - Quando existe algum erro relacionado a interação com a base de dados
+        FileNotFoundException - Quando tentamos ler ou escrever em um arquivo que não existe
+     */
+
 }
